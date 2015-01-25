cca92d08cbb9dec9da25aa446bdc48299ae18fdb
Lamba <pslamba@gmail.com>
2015-01-25 14:59:10 -0500

    v0.1.0 rewrite with angular, bootstrap, contenteditable

---
+++ server.js
@@ -0,0 +1,33 @@
+var 
+  express,
+	http,
+	loginErrorMessage,
+	cookieParser,
+	session,
+  port,
+	server,
+	todosVersion = "v0.2.0",
+	sid,
+
+	//functions
+	sendEmail;
+
+//link required node.js modules  
+express = require("express");	
+http = require("http");
+cookieParser = require("cookie-parser");
+session = require("express-session");
+
+port = 800;
+server = express();
+
+server.use(express.static(__dirname + "/"));
+server.use(cookieParser());
+server.use(session({
+	secret:'topsecret',
+	saveUninitialized:true,
+	resave:true
+}));
+
+http.createServer(server).listen(process.env.PORT || port);
+console.log("Server " + todosVersion + " is listening on port " + (process.env.PORT || port));
